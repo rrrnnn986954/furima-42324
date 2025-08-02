5709e9aa8a5e27a03d431b28b8581826a5df42e5
rrrnnn986954 <negima6954@gmail.com>
2025-08-02 11:07:04 +0900

    devise導入・マイグレート・バリデーション

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # @を含むかチェック
+  validates :email, format: { with: /@/, message: 'には@を含めてください' }
+
+  # パスワードの英数字混合チェック
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英字と数字の両方を含めてください' }, if: -> { password.present? }
+
+  # 全角のみ許可（ひらがな・カタカナ・漢字）
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
+
+  # 全角のみ許可（ひらがな・カタカナ・漢字）
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
+
+  # last_name_kanaカラムにカタカナのみ許可
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+
+  # first_name_kanaカラムにカタカナのみ許可
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+end
