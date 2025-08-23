00869e20413501e48a225bf5b76c4ed7b7e06696
rrrnnn986954 <negima6954@gmail.com>
2025-08-23 14:54:55 +0900

    ビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
 
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, sign_out_via: :delete
   root "items#index"
-  resources :items, only: [:new, :create, :index, :show]
+  resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
