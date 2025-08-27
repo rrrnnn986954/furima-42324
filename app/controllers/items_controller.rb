82d6aca89474979adfe66413146c287dfe74ed9c
rrrnnn986954 <negima6954@gmail.com>
2025-08-27 15:30:08 +0900

    ルーティング、コントローラー、一覧機能からのパス

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,13 +23,35 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: '商品情報を更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path, notice: '商品を削除しました'
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:item_name, :item_explanation, :category_id, :situation_id,
                                  :shipping_charge_id, :shipping_area_id, :delivery_time_id,
                                  :amount, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user == @item.user
   end
 end
