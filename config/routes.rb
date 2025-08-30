631b8673da7f6ab14a00ba4e9efefde66fce32a5
rrrnnn986954 <negima6954@gmail.com>
2025-08-30 20:17:06 +0900

    ルーティングへresources :ordersを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, sign_out_via: :delete
   root "items#index"
   resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :orders, only: [:index]
 end
