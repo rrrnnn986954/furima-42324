a9da7384c6f464f75f5baeff5785c1df456e6e4c
rrrnnn986954 <negima6954@gmail.com>
2025-08-30 20:14:35 +0900

    ordersコントローラーの作成、ルーティングにorders#indexの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'orders/index'
   devise_for :users, sign_out_via: :delete
   root "items#index"
   resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy]
