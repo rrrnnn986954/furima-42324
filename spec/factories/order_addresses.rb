742c3bdff9ffba4ab7dbeed9224c27d455a7aa9f
rrrnnn986954 <negima6954@gmail.com>
2025-09-08 21:35:04 +0900

    association :user     association :itemの削除

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -7,8 +7,5 @@ FactoryBot.define do
     building      { '柳ビル103' }
     phone_number  { '09012345678' }
     token         { 'tok_abcdefghijk00000000000000000' }
-
-    association :user
-    association :item
   end
 end
