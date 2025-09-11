a973d5c869b88db26f32ab6914a8e281e908c7ee
rrrnnn986954 <negima6954@gmail.com>
2025-09-11 20:39:45 +0900

    ログアウト修正_ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, sign_out_via: :delete
   root "items#index"
 
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+
   resources :items do
     resources :orders, only: [:index, :create]
   end
