8c2ae16ca5f5c9963f18981d9b4074ef3437ceed
rrrnnn986954 <negima6954@gmail.com>
2025-09-11 22:22:37 +0900

    削除＿商品購入ページのform_withにdata: { turbo: false } を追加、content_security_policy.rbに追加

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -23,10 +23,3 @@
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
 # end
-
-# Payjp用
-Rails.application.config.content_security_policy do |policy|
-  policy.script_src :self, :https, 'https://js.pay.jp'
-  policy.frame_src  :self, :https, 'https://js.pay.jp'
-end
-
